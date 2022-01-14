c6652051b0dba22c09c4172f53906563bcdb3fba
samuelliu30 <Yichen.Liu@amd.com>
2022-01-14 16:44:33 -0500

    Modify the input and output channels to 3

--- model.py
+++ model.py
@@ -6,15 +6,23 @@ class Net(nn.Module):
     def __init__(self, upscale_factor):
         super(Net, self).__init__()
 
-        self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
+        #self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
+        self.conv1 = nn.Conv2d(3, 64, (5, 5), (1, 1), (2, 2))
         self.conv2 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
-        self.conv3 = nn.Conv2d(32, 1 * (upscale_factor ** 2), (3, 3), (1, 1), (1, 1))
-        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+        # self.conv3 = nn.Conv2d(32, 1 * (upscale_factor ** 2), (3, 3), (1, 1), (1, 1))
+        self.conv3 = nn.Conv2d(32, 3, (3, 3), (1, 1), (1, 1))
+
+        
+        #Remove the pixel shuffle layer for now to run on RadeonML
+        #self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
 
     def forward(self, x):
         x = F.tanh(self.conv1(x))
         x = F.tanh(self.conv2(x))
-        x = F.sigmoid(self.pixel_shuffle(self.conv3(x)))
+        #x = F.sigmoid(self.pixel_shuffle(self.conv3(x)))
+
+        #Remove the pixel shuffle layer for now to run on RadeonML
+        x = F.sigmoid(self.conv3(x))
         return x
 
 
